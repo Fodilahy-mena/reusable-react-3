226e143c0369551efbccc5841285e1491281a29b
JEROME Emilien <jerome.emi@onja.org>
2020-11-27 11:28:35 +0300

    added disable in section of accordion components

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
 function Accordion({ data, position="top", disabled=[] }) {
   const [activeIndex, setActiveIndex] = useState(0)
@@ -37,8 +37,65 @@ function Accordion({ data, position="top", disabled=[] }) {
     </div>
   )
 }
+let AccordionContext = createContext()
+
+function AccordionCC({ children }) {
+  const [activeIndex, setActiveIndex] = useState(0)
+  return (
+    <div data-accordion>
+      {children.map((child, index) => {
+        return (
+          <AccordionContext.Provider
+            key={`section-${index}`}
+            value={{index, activeIndex, setActiveIndex}}
+          >
+            {child}
+          </AccordionContext.Provider>
+        )
+      })}
+    </div>
+  )
+}
+let SectionContext = createContext()
+function Section({ children, disabled = false,}) {
+  return (<SectionContext.Provider value={{disabled}}>
+            <div data-section>{children}</div>
+          </SectionContext.Provider>)
+}
+function Title({ children }) {
+  let {index, activeIndex, setActiveIndex } = useContext(AccordionContext);
+  let { disabled} = useContext(SectionContext)
+  let isActive = index === activeIndex
+  // const isDisabled = disabled.includes(index2)
+  // console.log(isDisabled)
+  
+  return (
+    <div
+      data-panel-title
+      className={
+        disabled ? 'disabled' : isActive ? 'expanded' : ''
+      }
+      onClick={() => {
+        if (!disabled) {
+          setActiveIndex(index)
+        }
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+function Content({ children }) {
+  let { index, activeIndex } = useContext(AccordionContext)
+  let isActive = index === activeIndex
+  return (
+    <div data-panel-content className={isActive ? 'expanded' : ''}>
+      {children}
+    </div>
+  )
+}
 function App() {
-  const data = [
+  /*const data = [
     {
       label: 'Paris',
       icon: '🧀',
@@ -54,10 +111,35 @@ function App() {
       icon: '🍷',
       content: <Description city="madrid" />,
     },
-  ]
+  ]*/
   return (
     <div className="App">
-      <Accordion data={data} position="bottom" disabled={[1]} />
+      <AccordionCC>
+        <Section>
+          <Title>
+            <span>🧀</span> Paris
+          </Title>
+          <Content>
+            <Description city="paris" />
+          </Content>
+        </Section>
+        <Section disabled>
+          <Title>
+            Lech <span>⛷</span>
+          </Title>
+          <Content>
+            <Description city="lech" />
+          </Content>
+        </Section>
+        <Section>
+          <Title>
+            Madrid <span>🍷</span>
+          </Title>
+          <Content>
+            <Description city="madrid" />
+          </Content>
+        </Section>
+      </AccordionCC>
     </div>
   )
 }
